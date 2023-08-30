971f06feadd0839d4189af261bf2a55d8099957e
johnrey <johnreymaiquez11@gmail.com>
2023-08-31 03:23:20 +0800

    change App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Contact from './components/Contact/Contact';
 import Footer from './components/footer/Footer';
 import Intro from './components/intro/Intro';
-import Navbar from './components/navbar/navbar';
+import Navbar from './components/navbar/Navbar';
 import Projects from './components/projects/Projects';
 import Skills from './components/skills/Skills';
 function App() {
